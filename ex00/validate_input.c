a2f08fabd3b651af5e17d1368973e5463a22ad89
Dmitrii Andrushchenko <dandrush@c1r16s8.42porto.com>
2026-02-07 15:13:00 +0000

    delivering validate_input.c

--- ex00/validate_input.c
+++ ex00/validate_input.c
@@ -6,8 +6,39 @@
 /*   By: dandrush <dandrush@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 14:17:37 by dandrush          #+#    #+#             */
-/*   Updated: 2026/02/07 14:21:11 by dandrush         ###   ########.fr       */
+/*   Updated: 2026/02/07 15:12:20 by dandrush         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int
+//#include <stdio.h>
+int	validate_input(char *str)
+{
+	int	i;
+	int	cnt;
+
+	i = 0;
+	cnt = 0;
+	while (str[i])
+	{
+		if (!((str[i] >= '1' && str[i] <= '4') || str[i] == ' '))
+			return (0);
+		if (str[i] >= '1' && str[i] <= '4')
+			cnt += 1;
+		i++;
+	}
+	if (cnt != 16)
+		return (0);
+	return (1);
+}
+/*
+int	main(void)
+{
+	int	res;
+
+	res = 0;
+	res = validate_input("7 2 3 4 1 2 3 4 1 2 3 4 1 2 3 4");
+	printf("%d\n", res);
+
+return (0);
+}
+*/
