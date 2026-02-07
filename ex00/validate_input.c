9df6646968f0b6b5a7b5c74dac75f05d8688ed9f
Dmitrii Andrushchenko <dandrush@c1r16s8.42porto.com>
2026-02-07 14:33:12 +0000

    drafting

--- ex00/validate_input.c
+++ ex00/validate_input.c
@@ -1,41 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   validate_input.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dandrush <dandrush@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/07 13:50:48 by dandrush          #+#    #+#             */
-/*   Updated: 2026/02/07 14:16:56 by dandrush         ###   ########.fr       */
+/*   Created: 2026/02/07 14:17:37 by dandrush          #+#    #+#             */
+/*   Updated: 2026/02/07 14:21:11 by dandrush         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#define N 4
-
-//Prototyping
-int	validate_input(char *str);
-void	ft_skyscraper(void);
-
-int	main(int argc, char **argv)
-{
-	if (argc != 2)
-	{
-		write(1, "Error\n", 6);
-		return (0);
-	}
-
-//int	validate_input(*str)
-
-
-//if(validate_input(argv[1]) == 0)
-	{
-		write(1, "Error\n", 6);
-		return (0);
-	}
-
-//void	ft_skyscraper(*arr);
-
-return (0);
-}
+int
