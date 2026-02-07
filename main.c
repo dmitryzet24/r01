2a2d4c5b6a0b909dd2564df0664f2e724e580671
Dmitrii Andrushchenko <dandrush@c1r16s8.42porto.com>
2026-02-07 14:03:40 +0000

    draft of main

---
+++ main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_scyscrapper.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dandrush <dandrush@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/07 13:50:48 by dandrush          #+#    #+#             */
+/*   Updated: 2026/02/07 14:00:53 by dandrush         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#define N 4
+
+//Prototyping
+int	validate_input(char *str);
+void	ft_skyscraper(void);
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+if(validate_input(argv[1]) == 0)
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+
+//void	ft_skyscraper(*arr);
+
+return (0);
+}
