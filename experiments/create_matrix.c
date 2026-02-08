c260a3b59dbef2285b49fcbb7295df78dc2afbd9
Dmitrii Andrushchenko <dandrush@c1r16s6.42porto.com>
2026-02-08 13:35:28 +0000

    create_matrix draft

--- experiments/create_matrix.c
+++ experiments/create_matrix.c
@@ -1,4 +1,30 @@
-int *create_matrix(*str)
+#include <unistd.h>
+
+int *ft_skyscraper(void)
 {
-    ///Addidng some code
+    int matrix[4][4] = {{1, 2, 3, 4}, {4, 3, 2, 1}, {1, 2, 3, 4}, {4, 3, 2, 1}};
+    int rw = 0;
+    int cl = 0;
+    char    c;
+
+    while (rw < 4)
+	{
+        cl = 0;
+		while (cl < 4)
+		{
+			c = matrix[rw][cl] + '0';
+			write(1, &c, 1);
+            if (cl < 3)
+                write(1, " ", 1);
+			cl++;
+		}
+        write(1, "\n", 1);
+		rw++;
+	}	
+}
+
+int main(void)
+{
+    ft_skyscraper();
+    return (0);
 }
